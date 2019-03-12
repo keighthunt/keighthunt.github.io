819d9cfff2fe99753150fa8d05dbfc6151fe32aa
Kate Hunt <katehunt@Kates-MacBook-Pro.local>
2019-03-12 21:50:12 +0000

    Setup

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+export default class Header extends Component {
+    render() {
+        let resumeData = this.props.resumeData;
+        return (
+            <div>
+                Header
+            </div>
+        );
+    }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,5 @@
+let resumeData = {
+
+}
+
+export default resumeData
